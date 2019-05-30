62c20e61c8735093c20c0a096222ca0625fd45bc
Harikrishnan <hkkrishn563@gmail.com>
2019-05-30 14:55:39 -0700

    Initialized Front End Construction

---
+++ url_shorten/client/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Component} from 'react'
+
+
+class App extends Component {
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+
+
+
+export default App;
