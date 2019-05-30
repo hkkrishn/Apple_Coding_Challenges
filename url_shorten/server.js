079995cc57ec10f7e87de3c336da42c53aee19d7
Harikrishnan <hkkrishn563@gmail.com>
2019-05-30 09:10:03 -0700

    intialized express and Node server

--- url_shorten/server.js
+++ url_shorten/server.js
@@ -1,1 +1,15 @@
-//Intilalize node.js code here
+//Tech Stack used
+//Node, React,Javascript HTML,CSS.MongoDB for the backend and UniqID to create a unique id via a hash function
+
+const express = require('express') // Initializes express middleware for us
+const app = express();
+
+//the path that we want to listen to
+//GET request
+app.get('/', (req, res) => {
+    res.send('Hello Apple')
+})
+
+//Since this project might me deployed live it can either use the process.env port or port 9000
+const port = process.env.PORT || 9000
+app.listen(port, () => console.log(`Server is running on port ${port}`));
